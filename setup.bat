f0133dfd0862d1d3729518935b04ca3d3234d045
hibiya inemuri <inemuri@hibiya.moe>
2026-01-21 18:23:55 +0900

    introduce uv

--- setup.bat
+++ setup.bat
@@ -1,12 +1,8 @@
 @ECHO OFF
 cd %~dp0
-if not exist .\venv (
-	ECHO CREATING VIRTUAL ENVIRONMENT
-	python -m venv venv
-)
-CALL venv\Scripts\activate
 
 ECHO Installing requirements
 ECHO python --version
-pip install -r requirements.txt
+ECHO uv --version
+uv sync
 PAUSE
